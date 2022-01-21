505af677c22f027bb188e79e007afb3b19eb46d9
kelesemre <emrekeless@gmail.com>
2022-01-21 23:42:44 +0100

    Frontent Client has been added

---
+++ Frontends/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name ="Your email address")]
+        public string Email { get; set; }
+        [Display(Name = "Your password")]
+        public string Password { get; set; }
+        [Display(Name = "remember me")]
+        public bool IsRemember { get; set; }
+    }
+}
