914c2e59ac389f2ca09c2703ff588b72b86c32ae
kelesemre <emrekeless@gmail.com>
2022-02-09 00:14:23 +0100

    Fluentvalidation-added

---
+++ Frontends/FreeCourse.Web/Validators/CourseCreateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FreeCourse.Web.Models.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Validators
+{
+    public class CourseCreateValidator: AbstractValidator<CourseCreateInput>
+    {
+        public CourseCreateValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name must have a value");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description must have a value");
+            RuleFor(x => x.Feature.Duration).InclusiveBetween(1,int.MaxValue).WithMessage("Duration must have a value");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price must have a value").ScalePrecision(2, 6).WithMessage("wrong format");  // ₺₺₺₺.₺₺
+        }
+    }
+}
